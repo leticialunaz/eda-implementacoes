73ccc9227eca57eb3277d1faa6667e87295aabac
Letícia Luna <leticialunadb@gmail.com>
2025-09-01 09:24:41 -0300

    finalização da implementação do Heap

--- Heap.java
+++ Heap.java
@@ -24,4 +24,74 @@ public class Heap {
         return int((indice - 2)/2);
     }
 
+    public void add(int elemento){
+    	this.heap[++tail] = elemento;
+	
+	int cont = tail;
+	while(cont > 0 && this.heap[cont] > this.heap[parent(cont)]){
+	    swap(cont, parent(cont));
+	    cont = parent(cont);
+	}
+    }
+
+
+    public int remove(){
+    	int element = this.heap[0];
+	swap(0, tail);
+	this.tail--;
+
+	heapify(0);
+
+	return element;
+    }
+
+
+    public void heapify(int index){
+    	if(isLeaf(index) || !isValidIndex(index))
+	    return;
+
+	int maxIndex = maxIndex(index, right(index), left(index));
+
+	if(maxIndex != index){
+	    swap(maxIndex, index);
+	    heapify(maxIndex);
+	}
+    }
+
+
+    public int maxIndex(int index, int right, int left){
+    	if(isValidIndex(left) && this.heap[index] > this.heap[left]){
+	    if(isValidIndex(right) && this.heap[index] > this.heap[right])
+	    	return index;
+	    else if(isValidIndex(right) && this.heap[index] < this.heap[right])
+		return right;
+	    else
+		return index;
+
+	} else if(isValidIndex(left) && this.heap[index] < this.heap[left]){
+	    if(isValidIndex(right) && this.heap[index] > this.heap[right])
+		return left;
+	    else if(isValidIndex(right) && this.heap[left] < this.heap[right])
+		return right;
+	}   else
+		return left;
+	return index;		
+    }
+
+
+    public boolean isValidIndex(int i){
+    	return (i >= 0 && i <= tail);
+    }
+
+    public boolean isLeaf(int index){
+    	return (index > parent(index) && index <= tail);
+    }
+
+
+
+    public void swap(int i, int j){
+    	int aux = this.heap[i];
+	this.heap[i] = this.heap[j];
+	this.heap[j] = aux;
+    }
 }
